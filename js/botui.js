da7b7a65f4a8ec7ab57f104aef60777bb90eca64
xuyong <2505785045@qq.com>
2020-08-06 09:10:43 +0800

    Site updated: 2020-08-06 09:10:42

--- js/botui.js
+++ js/botui.js
@@ -26,11 +26,11 @@ function bot_ui_ini() {
     }).then(function () {
         botui.message.add({
             delay: 1100,
-            content: "这里是 Mashiro"
+            content: "这里是 红狼"
         }).then(function () {
             botui.message.add({
                 delay: 1100,
-                content: "一个可爱的蓝孩子~"
+                content: "一个刚满18岁善良的蓝孩子~"
             }).then(function () {
                 botui.action.button({
                     delay: 1600,
@@ -64,44 +64,44 @@ function bot_ui_ini() {
         },
         secondpart = function () {
             botui.message.add({
-                delay: 1500,
-                content: "目前就读于上海财经大学"
-            }).then(function () {
-                botui.message.add({
-                    delay: 1500,
-                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
-                }).then(function () {
-                    botui.message.add({
-                        delay: 1200,
-                        content: "因为数据分析也需要Coder嘛"
-                    }).then(function () {
-                        botui.message.add({
-                            delay: 1500,
-                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
-                        }).then(function () {
-                            botui.message.add({
-                                delay: 1500,
-                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
-                            }).then(function () {
-                                botui.message.add({
+            //     delay: 1500,
+            //     content: "目前就读于上海财经大学"
+            // }).then(function () {
+            //     botui.message.add({
+            //         delay: 1500,
+            //         content: "向往技术却误入商科，但后来喜欢上了经济学…"
+            //     }).then(function () {
+            //         botui.message.add({
+            //             delay: 1200,
+            //             content: "因为数据分析也需要Coder嘛"
+            //         }).then(function () {
+            //             botui.message.add({
+            //                 delay: 1500,
+            //                 content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
+            //             }).then(function () {
+            //                 botui.message.add({
+            //                     delay: 1500,
+            //                     content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
+            //                 }).then(function () {
+                                // botui.message.add({
                                     delay: 1800,
-                                    content: "喜欢画画，希望有一天能够被称为画师"
+                                    content: "喜欢电影,动漫,音乐,健身(脑补),做饭(脑补)"
                                 }).then(function () {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "为什么叫Mashiro呢？ 🤔",
+                                            text: "还有呢？ 🤔",
                                             value: "why-mashiro"
                                         }]
                                     }).then(function (a) {
-                                        thirdpart()
+                                        fifthpart()
                                     })
                                 })
-                            })
-                        })
-                    })
-                })
-            })
+                            // })
+            //             })
+            //         })
+            //     })
+            // })
         },
         thirdpart = function () {
             botui.message.add({
@@ -143,7 +143,7 @@ function bot_ui_ini() {
         fifthpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
+                content: "那么，仔细看看我的博客吧？ ^_^"
             }).then(function () {
                 botui.message.add({
                     delay: 1600,
